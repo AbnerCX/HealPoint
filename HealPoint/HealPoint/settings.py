ee32f70d1025e17897c05e226cab5b02aa7ae8b5
Abner G. Arce <abner234.lg@gmail.com>
2025-06-28 20:49:30 -0600

    refactor: replace dotenv with decouple for environment variable management

--- HealPoint/HealPoint/settings.py
+++ HealPoint/HealPoint/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from datetime import timedelta
 import os
-from dotenv import load_dotenv
+from decouple import config
 from pathlib import Path
 
-load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,11 +84,11 @@ WSGI_APPLICATION = 'HealPoint.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'UniRouteDB'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
