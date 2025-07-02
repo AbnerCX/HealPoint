8c2aaadda5c304ef6b3a3bc9c191d06b2acbd78e
Abner G. Arce <abner234.lg@gmail.com>
2025-07-02 13:18:11 -0600

    feat: register Appointment model in admin with display and filter options

--- HealPoint/appointments/admin.py
+++ HealPoint/appointments/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("patient", "doctor", "store", "start_time", "end_time", "status")
+    list_filter = ("doctor", "store", "status")
