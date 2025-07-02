bbed42458d851746451a127b82dc5433e7f40202
Abner G. Arce <abner234.lg@gmail.com>
2025-07-01 23:02:11 -0600

    feat: register Patient model in admin with search fields and list display

--- HealPoint/patients/admin.py
+++ HealPoint/patients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    search_fields = ("names", "father_surname", "mother_surname",)
+    list_display =("full_name", "age", "birth_date", "gender", "created_at")
